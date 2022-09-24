526941cb2f9839ec49e07dd720ac29badb7028a7
liuming <liuming@wuji.co>
2022-09-24 17:15:17 +0800

    format

--- Example/RCToolKit/ViewController.swift
+++ Example/RCToolKit/ViewController.swift
@@ -6,49 +6,45 @@
 //  Copyright (c) 2022 liuming. All rights reserved.
 //
 
-import UIKit
 import RCToolKit
+import UIKit
 class ViewController: UIViewController {
-    private let queue =  DispatchQueue(label: "com.hello.kit")
+    private let queue = DispatchQueue(label: "com.hello.kit")
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         RCLog.rc.setLogLevel(level: .debug)
         RCLog.rc.addLogger(logger: RCConsoleLog())
-        RCLog.rc.addLogger(logger:RCFileLog())
+        RCLog.rc.addLogger(logger: RCFileLog())
         if #available(iOS 13.0, *) {
             RCLog.rc.addLogger(logger: RCWebsocketLog())
         } else {
             // Fallback on earlier versions
         }
-        self.view.rc.x = 10.aptValue;
+        self.view.rc.x = 10.aptValue
 //        queue.async {
-        RCLog.rc.logD(message: "走你！！！！");
-        RCLog.rc.LogT(tag: "xxx1");
-//        sleep(2)åß
-        RCLog.rc.LogT(tag: "xxx1");
+        RCLog.rc.logD(message: "走你！！！！")
         RCLog.rc.LogT(tag: "xxx1")
-        RCLog.rc.removeLogT(tag: "xxx1");
+//        sleep(2)åß
+        RCLog.rc.LogT(tag: "xxx1")
+        RCLog.rc.LogT(tag: "xxx1")
+        RCLog.rc.removeLogT(tag: "xxx1")
 //        for i in 0..<1000 {
 //            RCLog.rc.logD(message: "this message index \(i)");
 //        }
-        
+
         self.view.rc.bgColor(.white).clipsToBounds()
-        
-        
-        UIImageView().rc
-            .addToSuperView(self.view)
-            .bgColor(.blue)
-            .frame(CGRect(x: 10, y: 10, width: 100, height: 100))
-            .bgColor(.blue)
-            .image(nil)
-            .cornerRadius(20.0)
+
+        UIImageView().rc.addToSuperView(self.view)
+                        .bgColor(.blue)
+                        .frame(CGRect(x: 10, y: 10, width: 100, height: 100))
+                        .bgColor(.blue)
+                        .image(nil)
+                        .cornerRadius(20.0)
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
 }
-
