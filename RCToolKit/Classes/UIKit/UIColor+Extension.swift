43f414136635eb66a5284fb155dfdd3150fb7ed0
liuming <liuming@wuji.co>
2022-10-21 11:07:12 +0800

    UIColor 扩展

---
+++ RCToolKit/Classes/UIKit/UIColor+Extension.swift
@@ -0,0 +1,16 @@
+//
+//  UIColor+Extension.swift
+//  RCToolKit
+//
+//  Created by yoyo on 2022/10/21.
+//
+
+import Foundation
+extension UIImage: ExtensionCompatibleValue {}
+public extension ExtensionWrapper where Base == UIColor.Type {
+    
+    func rbga(r:UInt,g:UInt,b:UInt,a:UInt)->UIColor {
+        return UIColor(red: CGFloat(r) / 255.0, green: CGFloat(g) / 255.0, blue: CGFloat(b) / 255.0, alpha: CGFloat(a) / 255.0)
+    }
+    
+}
