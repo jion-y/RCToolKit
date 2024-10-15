7ea0b085fad7991d59758597af961453ecb21c1b
liuming <3165440298@qq.com>
2024-10-15 19:01:58 +0800

    放开外部访问

--- RCToolKit/Classes/StoreKit/RCStoreKit2.swift
+++ RCToolKit/Classes/StoreKit/RCStoreKit2.swift
@@ -10,8 +10,8 @@ import StoreKit
 
 @available(iOS 13, *)
 // 协程封装下，方便上传使用
-public class RCStoreKit2 {
-    class func getProductInfo(_ productIds: Set<String>, completion: @escaping (RetrieveResults) -> Void) async -> RetrieveResults {
+open class RCStoreKit2 {
+   public class func getProductInfo(_ productIds: Set<String>) async -> RetrieveResults {
         await withCheckedContinuation { continuation in
             let _ = RCStoreKit.getProductInfo(productIds) { result in
                 continuation.resume(returning: result)
@@ -19,7 +19,7 @@ public class RCStoreKit2 {
         }
     }
     
-    class func purchaseProduct(_ productId: String, quantity: Int = 1, atomically: Bool = true, applicationUsername: String = "", simulatesAskToBuyInSandbox: Bool = false, completion: @escaping (PurchaseResult) -> Void) async -> PurchaseResult {
+    public class func purchaseProduct(_ productId: String, quantity: Int = 1, atomically: Bool = true, applicationUsername: String = "", simulatesAskToBuyInSandbox: Bool = false) async -> PurchaseResult {
         await withCheckedContinuation { continuation in
             RCStoreKit.purchaseProduct(productId, quantity: quantity, atomically: atomically, applicationUsername: applicationUsername, simulatesAskToBuyInSandbox: simulatesAskToBuyInSandbox) { result in
                 continuation.resume(returning: result)
@@ -27,7 +27,7 @@ public class RCStoreKit2 {
         }
     }
 
-    class func purchaseProduct(_ product: SKProduct, quantity: Int = 1, atomically: Bool = true, applicationUsername: String = "", simulatesAskToBuyInSandbox: Bool = false, paymentDiscount: PaymentDiscount? = nil, completion: @escaping (PurchaseResult) -> Void) async -> PurchaseResult {
+    public class func purchaseProduct(_ product: SKProduct, quantity: Int = 1, atomically: Bool = true, applicationUsername: String = "", simulatesAskToBuyInSandbox: Bool = false, paymentDiscount: PaymentDiscount? = nil) async -> PurchaseResult {
         await withCheckedContinuation { continuation in
             RCStoreKit.purchaseProduct(product, quantity: quantity, atomically: atomically, applicationUsername: applicationUsername, simulatesAskToBuyInSandbox: simulatesAskToBuyInSandbox) { result in
                 continuation.resume(returning: result)
@@ -35,7 +35,7 @@ public class RCStoreKit2 {
         }
     }
 
-    class func restorePurchases(atomically: Bool = true, applicationUsername: String = "", completion: @escaping (RestoreResults) -> Void) async -> RestoreResults {
+    public class func restorePurchases(atomically: Bool = true, applicationUsername: String = "") async -> RestoreResults {
         await withCheckedContinuation { continuation in
             RCStoreKit.restorePurchases(atomically: atomically, applicationUsername: applicationUsername) { result in
                 continuation.resume(returning: result)
